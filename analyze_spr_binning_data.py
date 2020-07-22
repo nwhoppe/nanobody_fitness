220e2fbbc81c48bc90ec1be1bbe002ab5624a890
nwhoppe <nwhoppe@wisc.edu>
2020-07-21 17:05:58 -0700

    trying seaborn

--- analyze_spr_binning_data.py
+++ analyze_spr_binning_data.py
@@ -17,9 +17,14 @@ if __name__ == '__main__':
     # parser.add_argument()
     args = parser.parse_args()
     sensorgram_df = pd.read_csv(args.sensorgram_raw_binning_data, sep='\t', encoding='iso-8859-1', index_col=0)
+    # first assocation starts at about 200 sec and dissocation ends at 500
+    post_loading_df = sensorgram_df.loc[sensorgram_df.index > 200]
+    association_df = post_loading_df.loc[post_loading_df.index < 400]
     # sensorgram_df.columns = list(itertools.product(association_times, ['time', 'response']))
-    sens_plot = sns.lineplot(x=sensorgram_df.index, y=sensorgram_df.iloc[:,0])
-    fig = sens_plot.get_figure()
-    fig.savefig('test.png')
+    sns.set_style("white")
+    sns.despine()
+    sns_plot = sns.lineplot(x=association_df.index, y=association_df.iloc[:, 0])
+    t = sns_plot.get_figure()
+    t.savefig('test.png')
 
 
